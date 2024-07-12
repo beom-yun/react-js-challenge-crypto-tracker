88cb695367d5613249eb17717f8898ef11085bf1
beom-yun <scds0326@naver.com>
2024-07-13 01:46:14 +0900

    Deploying

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme, themeState } from './theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot, useRecoilState } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
